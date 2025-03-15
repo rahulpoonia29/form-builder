358e1b12e96c38ab68212fa19a18f7ea0a001ac2
Rahul <chattychidiya@gmail.com>
2025-03-15 18:49:23 +0530

    feat: update component list

--- src/components/form-components/index.ts
+++ src/components/form-components/index.ts
@@ -1,16 +1,26 @@
 import { FormComponentsCategory } from '@/types/form';
 import textInputConfig from './inputs/text-input';
 import passwordInputConfig from './inputs/password-input';
+import emailInputConfig from './inputs/email-input';
+import numberInputConfig from './inputs/number-input';
+import textareaInputConfig from './inputs/textarea-input';
+import selectInputConfig from './selectors/select-input';
 
 // Registry of all form components
 const formComponents: FormComponentsCategory[] = [
   {
     name: 'Input Fields',
-    components: [textInputConfig, passwordInputConfig],
+    components: [
+      textInputConfig,
+      passwordInputConfig,
+      emailInputConfig,
+      numberInputConfig,
+      textareaInputConfig,
+    ],
   },
   {
     name: 'Selectors',
-    components: [],
+    components: [selectInputConfig],
   },
   {
     name: 'Buttons',
